0c3447462b62566a43d4f655337c2bf93f6c1069
markDeubzer <mark.deubzer@gmail.com>
2026-04-22 15:45:04 +0200

    some clean up

--- erdstall_admin_gui/main.py
+++ erdstall_admin_gui/main.py
@@ -1,41 +1,25 @@
 from __future__ import annotations
 import sys
-import time
 import qdarktheme
 
-t0 = time.time()
-print("1 start")
-
 from PySide6.QtWidgets import QApplication
-print("2 QApplication", time.time() - t0)
-
 from PySide6.QtCore import QTimer
-print("3 QTimer", time.time() - t0)
-
 from PySide6.QtGui import QIcon
-print("4 QIcon", time.time() - t0)
-
 from erdstall_admin_gui.windows.splash_screen import SplashScreen
-print("5 SplashScreen", time.time() - t0)
-
 from erdstall_admin_gui.windows.main_window import MainWindow
-print("6 MainWindow", time.time() - t0)
-
 
 def main() -> int:
-    print("7 creating app", time.time() - t0)
     app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon("puplic/admin_icon.png"))
     qdarktheme.setup_theme("dark")
 
-    print("8 creating splash", time.time() - t0)
     splash = SplashScreen()
     splash.show()
     app.processEvents()
 
-    print("9 creating main window", time.time() - t0)
+
     main_window = MainWindow()
 
-    print("10 main window created", time.time() - t0)
 
     progress_timer = QTimer()
     progress_value = {"value": 0}
@@ -50,8 +34,6 @@ def main() -> int:
 
     progress_timer.timeout.connect(update_progress)
     progress_timer.start(100)
-
-    print("11 entering event loop", time.time() - t0)
     return app.exec()
 
 
